35c2054b7f995f6c6fec0af14bed377d1a223ec4
ShaykhJake <ShaykhJake@gmail.com>
2020-04-26 07:36:32 -0400

    New Commit

--- pidginforge/settings.py
+++ pidginforge/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-# import django_heroku
+import django_heroku
 # disabling django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -282,4 +282,4 @@ WEBPACK_LOADER = {
 }
 
 # Activate Django-Heroku
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
